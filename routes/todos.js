2d9ed6357d3544fab511edf0e175e60099f70952
mursal-r <157185506+mursal-r@users.noreply.github.com>
2024-02-21 23:36:09 -0800

    Add files via upload

---
+++ routes/todos.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+var todosCtrl = require('../controllers/todos');
+
+ // All actual paths start with "/todos"
+
+ // will be a GET for /todos:
+router.get('/', todosCtrl.index);
+
+// GET /todos/:id
+router.get('/:id', todosCtrl.show);
+
+module.exports = router;
